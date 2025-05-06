26f8a935f495950e1a627546dff59c30aa3e4221
Linsei <development.tanuki2024@gmail.com>
2025-05-06 18:27:06 +0900

    デバッグコードを削除

--- examples/write_char.rs
+++ examples/write_char.rs
@@ -1,7 +1,7 @@
 use memmap2::MmapMut;
 
 fn main() {
-    let mut mmap = MmapMut::map_anon(usize::MAX).expect("mmap");
+    let mut mmap = MmapMut::map_anon(4096).expect("mmap");
 
     mmap[0] = b'A';
     println!("{}", mmap[0] as char);
